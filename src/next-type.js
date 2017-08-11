247ea1b7b1e9b3bc6105e7a968168ebb28a52052
afeiship <1290657123@qq.com>
2017-08-11 22:27:45 +0800

    optmize

--- src/next-type.js
+++ src/next-type.js
@@ -2,14 +2,21 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
-  var toString = nx.class2type.toString;
+  var class2type = {};
+  var toString = class2type.toString;
+  var javascriptType = 'Boolean Number String Function Array Date RegExp Object Error';
+
+  //populate class2type map:
+  javascriptType.split(' ').forEach(function (inName) {
+    class2type['[object ' + inName + ']'] = inName.toLowerCase()
+  });
 
   nx.type = function (inObj) {
     if (inObj && nx.isFunction(inObj.type)) {
       return inObj.type();
     }
     return inObj == null ? String(inObj) :
-      nx.class2type[toString.call(inObj)] || 'object';
+      class2type[toString.call(inObj)] || 'object';
   };
 
   if (typeof module !== 'undefined' && module.exports) {
